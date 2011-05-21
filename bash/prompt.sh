9da2e03ad07922e384be6fb5266316c6ef4c6008
Hector Parra <wizbcn@gmail.com>
2011-05-21 17:54:10 +0200

    Update prompt colors

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #PROMPT
-export PS1="\[${COLOR_LIGHT_GRAY}\]\u: \[${COLOR_LIGHT_GREEN}\]\w\[${COLOR_LIGHT_BLUE}\]\$(__git_ps1) \[${COLOR_LIGHT_GREEN}\]\$\[${COLOR_NONE}\] " # primary prompt
+export PS1="\[${COLOR_LIGHT_GREEN}\]\u\[${COLOR_LIGHT_GRAY}\]: \[${COLOR_LIGHT_BLUE}\]\w\[${COLOR_YELLOW}\]\$(__git_ps1) \[${COLOR_LIGHT_GREEN}\]\$\[${COLOR_NONE}\] " # primary prompt
