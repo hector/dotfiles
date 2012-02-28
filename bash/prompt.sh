1596826fc9542da498c0dc9a58a88031af1725e6
Hector Parra <wizbcn@gmail.com>
2012-02-28 05:15:41 +0100

    Prompt color tweaking

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #PROMPT
-export PS1="\[${COLOR_LIGHT_GREEN}\]\u\[${COLOR_LIGHT_GRAY}\]: \[${COLOR_LIGHT_BLUE}\]\w\[${COLOR_YELLOW}\]\$(__git_ps1) \[${COLOR_LIGHT_GREEN}\]\$\[${COLOR_NONE}\] " # primary prompt
+export PS1="\[${COLOR_GREEN}\]\u\[${COLOR_LIGHT_GRAY}\]: \[${COLOR_BLUE}\]\w\[${COLOR_BROWN}\]\$(__git_ps1) \[${COLOR_GREEN}\]\$\[${COLOR_NONE}\] " # primary prompt
