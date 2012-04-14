61d497515334b3e9e3718596ee9e5621d839fef1
Hector Parra <wizbcn@gmail.com>
2012-04-15 00:44:49 +0200

    Optimize search of files using find

--- ruby/rvm.sh
+++ ruby/rvm.sh
@@ -1,4 +1,6 @@
 # This loads RVM into a shell session.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+# Add RVM to PATH for scripting
+[[ -s "$HOME/.rvm/bin" ]] && PATH=$PATH:$HOME/.rvm/bin 
 # RVM completion
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
