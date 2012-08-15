275fbf9d2d73c1190cb194d2cd25ec2ca8d1061c
Hector Parra <wizbcn@gmail.com>
2012-08-15 16:38:46 +0200

    FIX don't use brew if it not installed

--- bash/homebrew.sh
+++ bash/homebrew.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export HOMEBREW_PATH=`brew --prefix`
-
 if hash brew 2>&-; then 
-	PATH=$HOMEBREW_PATH/bin:$HOMEBREW_PATH/sbin:$PATH
+	PATH=`brew --prefix`/bin:`brew --prefix`/sbin:$PATH
 fi
