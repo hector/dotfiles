e02ea5f478984faf03572990d453d2285d5e38ad
Hector Parra <wizbcn@gmail.com>
2012-08-26 15:59:24 +0200

    Update rvm loading

--- ruby/rvm.sh
+++ ruby/rvm.sh
@@ -1,6 +1,6 @@
-# This loads RVM into a shell session.
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 # Add RVM to PATH for scripting
-[[ -s "$HOME/.rvm/bin" ]] && PATH=$PATH:$HOME/.rvm/bin 
+PATH=$PATH:$HOME/.rvm/bin
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 # RVM completion
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
