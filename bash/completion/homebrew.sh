127819810245b96336a380e8c2de0844743ab9a5
Hector Parra <wizbcn@gmail.com>
2012-10-02 02:35:49 +0200

    Update to bash-completion2

--- bash/completion/homebrew.sh
+++ bash/completion/homebrew.sh
@@ -2,10 +2,12 @@
 
 # Load Homebrew completion scripts if it is installed
 if hash brew 2>&-; then 
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
+  if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+    . $(brew --prefix)/share/bash-completion/bash_completion
   fi
-else # brew install bash-completion
+  # We want it to verbously fail so installing is enforced:
+  # brew install https://raw.github.com/Homebrew/homebrew-versions/master/bash-completion2.rb
+else
 	# Keep compatibilty for OSX without homebrew and other UNIX systems
 	for script in $(find $DOTFILES/bash/completion -type f -name "*.completion"); do source $script; done
 fi
