3a14a3e4d70975031aa0c31aef191b3571f323e1
Hector Parra <wizbcn@gmail.com>
2012-10-17 19:14:23 +0200

    Fix completion issues

--- bash/completion/homebrew.sh
+++ bash/completion/homebrew.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+FORCE_COMPLETION_SCRIPTS=false
+
 # Load Homebrew completion scripts if it is installed
-if hash brew 2>&-; then 
-  if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
-    . $(brew --prefix)/share/bash-completion/bash_completion
+if hash brew 2>&-; then 	
+  if [ -f $(brew --prefix)/etc/bash_completion ]; then
+	# brew install bash-completion
+    . $(brew --prefix)/etc/bash_completion
+  elif [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+	# brew install https://raw.github.com/Homebrew/homebrew-versions/master/bash-completion2.rb
+	. $(brew --prefix)/share/bash-completion/bash_completion
+  else
+	FORCE_COMPLETION_SCRIPTS=true
   fi
-  # We want it to verbously fail so installing is enforced:
-  # brew install https://raw.github.com/Homebrew/homebrew-versions/master/bash-completion2.rb
 else
-	# Keep compatibilty for OSX without homebrew and other UNIX systems
-	for script in $(find $DOTFILES/bash/completion -type f -name "*.completion"); do source $script; done
+  FORCE_COMPLETION_SCRIPTS=true
+fi
+
+# Keep compatibilty for OSX without homebrew (with completion packages installed) and other UNIX systems
+if $FORCE_COMPLETION_SCRIPTS; then
+  for script in $(find $DOTFILES/bash/completion -type f -name "*.completion"); do source $script; done
 fi
