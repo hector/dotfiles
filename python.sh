1b68050f529613467ff784e4a5ba31592dc529c6
Hector Parra <wizbcn@gmail.com>
2013-05-21 13:59:28 +0200

    fix typo

--- python.sh
+++ python.sh
@@ -18,7 +18,7 @@ alias pip_ls="pip search . | egrep -B1 'INSTALLED|LATEST'"
 # fi
 
 ########### Essentia exports ###########
-if [ -d /essentia/third_party]; then
+if [ -d /essentia/third_party ]; then
 	export ESSENTIA_THIRDPARTY=/essentia/third_party
 	export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ESSENTIA_THIRDPARTY/lib
 	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$ESSENTIA_THIRDPARTY/pkgconfig
