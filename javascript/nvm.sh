02e4e01d7222cdb0ce85c125f25995dd255ad0b9
Hector Parra <wizbcn@gmail.com>
2013-05-21 13:56:18 +0200

    nvm is gone

--- javascript/nvm.sh
+++
@@ -1,4 +0,0 @@
-if [ -d "$HOME/.nvm" ]; then
-	[[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh" # Load NVM into shell session
-	[[ -r $HOME/.nvm/bash_completion ]] && . $HOME/.nvm/bash_completion # NVM completion	
-fi
