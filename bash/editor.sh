754ad120fe6b04adabea6d095d348f64a97c79d0
Hector Parra <hector@parra.cat>
2013-06-01 02:15:56 +0200

    Add sublime text as EDITOR

--- bash/editor.sh
+++ bash/editor.sh
@@ -1,5 +1,6 @@
 # Set the default editor to launch from terminal
-if hash mate 2>&-; then export EDITOR=mate
+if hash mate 2>&-; then export EDITOR='mate -w'
+elif hash subl 2>&-; then export EDITOR='subl -w'	
 elif hash nano 2>&-; then export EDITOR=nano
 elif hash vim 2>&-; then export EDITOR=vim	
 else export EDITOR=vi
