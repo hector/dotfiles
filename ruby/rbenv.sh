72fc8670c85ca924f531365b53884eb46d7421d4
Hector Parra <hector@parra.cat>
2013-12-10 17:32:46 +0100

    Differentiate for homebrew-installed rbenv and pulled from git

--- ruby/rbenv.sh
+++ ruby/rbenv.sh
@@ -1,4 +1,7 @@
-if [ -d "$HOME/.rbenv" ]; then
+if which rbenv > /dev/null; then
+	export RBENV_ROOT=/usr/local/var/rbenv
+	eval "$(rbenv init -)";
+elif [ -d "$HOME/.rbenv" ]; then
 	export PATH="$HOME/.rbenv/bin:$PATH"
 	eval "$(rbenv init -)";
 fi
