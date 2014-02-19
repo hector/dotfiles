922ba6e7a6d4ae21ee833d878e769a5daa8f9414
Hector Parra <hector@parra.cat>
2014-02-19 21:59:07 +0100

    Add virtualenv and virtualenvwrapper

--- python.sh
+++ python.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 alias pylab='ipython --pylab'
+# Alias for listing eggs and if new versions exist
+alias pip_ls="pip search . | egrep -B1 'INSTALLED|LATEST'"
+
+# Set virtualenvs folder for virtualenvwrapper
+if [ -f "/usr/local/bin/virtualenvwrapper.sh" ]; then
+	export WORKON_HOME=$HOME/.venvs
+	export PROJECT_HOME=$PROJECTS
+	source /usr/local/bin/virtualenvwrapper.sh
+fi
 
 # Pythonbrew import
 [[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc
@@ -10,9 +19,6 @@ if [ -d "$HOME/.pyenv" ]; then
 	export PATH="$HOME/.pyenv/bin:$PATH"
 	if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 fi
-
-# Alias for listing eggs and if new versions exist
-alias pip_ls="pip search . | egrep -B1 'INSTALLED|LATEST'"
 
 # Add folders to import modules
 # if [ -d "/usr/local/lib/python2.6/site-packages" ]; then
