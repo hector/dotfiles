4db87a6fd0fc350a95449cbaed8728d062aab17e
Hector Parra <hector@parra.cat>
2014-04-26 19:43:39 +0200

    Add Heroku Toolbelt

--- bash/misc.sh
+++ bash/misc.sh
@@ -7,13 +7,16 @@ export HISTFILESIZE=3000
 # MYSQL
 [[ -s "/usr/local/mysql/bin" ]] && export PATH=/usr/local/mysql/bin:$PATH
 
-#PATH
+# PATH
 [[ -s "$HOME/.bin" ]] && export PATH=$HOME/.bin:$PATH
 
-#SOURCES
+# SOURCES
 # Load user script extensions
 [[ -s "$HOME/scripts" ]] && for script in $(find "$HOME/.scripts" -type f -name "*.sh"); do source $script; done
 
-#LOCALE
+# LOCALE
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
+# Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
