54a936817e0ccd4bd3e086465bf0d7d36db66f42
Hector Parra <hector@parra.cat>
2014-07-17 12:18:11 +0200

    Create an alias to upgrade homebrew casks

--- bash/homebrew.sh
+++ bash/homebrew.sh
@@ -3,3 +3,7 @@
 if hash brew 2>&-; then 
 	PATH=`brew --prefix`/bin:`brew --prefix`/sbin:$PATH
 fi
+
+# Temporal alias to upgrade casks (since 'brew cask upgrade' does not exist)
+# Old version won't be automatically removed. Go to /opt/homebrew-cask/Caskroom to do so
+alias brew-cask-upgrade='for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done'
