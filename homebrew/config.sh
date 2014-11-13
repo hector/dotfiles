4d2d50154264b6e739740daf84121aabcc8133e9
Hector Parra <hector@parra.cat>
2014-11-13 01:48:25 +0100

    Add Brewfile and Caskfiles

--- homebrew/config.sh
+++ homebrew/config.sh
@@ -3,14 +3,16 @@
 if hash brew 2>&-; then 
 	PATH=`brew --prefix`/bin:`brew --prefix`/sbin:$PATH
   
-  alias brew-cleanall='brew cleanup && brew prune'
+  alias brew-cleanall='brew cleanup && brew purge'
   
-  ### Hombrew-cask
-  
+  ### Homebrew-cask
+
   export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+  alias cask='brew cask'
   
   # Temporal alias to upgrade casks (since 'brew cask upgrade' does not exist)
   # Old version won't be automatically removed. Go to /opt/homebrew-cask/Caskroom to do so
-  alias brew-cask-upgrade='for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done'
+  alias brew-cask-upgrade='for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done'  
   
 fi
