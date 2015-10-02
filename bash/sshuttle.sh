841711a85e681cc6c7924ab6c53f459de35afee9
Hector Parra <hector@parra.cat>
2015-10-02 11:16:12 +0200

    Update sshuttle

--- bash/sshuttle.sh
+++ bash/sshuttle.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# sshuttle helpers from https://github.com/teohm/dotfiles/blob/master/.bashrc.d/sshuttle_helpers
-# with some additions
+# sshuttle helpers from https://github.com/teohm/dotfiles/blob/master/public/.functions.d/sshuttle.bash
+# with some personal additions
 
 # set default SSH server:
 #   user@hostname or a host in ~/.ssh/config
 TNL_SERVER=default_ssh
+
+# tunnel all traffic including DNS
+alias tnl="sshuttle --dns -vr $TNL_SERVER 0/0"
 
 # tunnel only DNS
 alias tnldns='sshuttle --dns $TNL_SERVER 0/0'
@@ -19,7 +22,7 @@ alias tnlreset='sudo ipfw -q -f flush'
 #  tnlhost myfavouritehost
 #  tnlhost # connects to default_ssh
 #
-function tnl() {
+function tnlhost() {
 	local HOST=$TNL_SERVER
 	if ! test -z "$1"; then HOST=$1; fi
 	sshuttle --dns -r $HOST 0/0
@@ -28,8 +31,8 @@ function tnl() {
 # tunnel only port 80, including DNS.
 #
 # Examples:
-#  tnlhost myfavouritehost
-#  tnlhost # connects to default_ssh
+#  tnlweb myfavouritehost
+#  tnlweb # connects to default_ssh
 #
 function tnlweb() {
 	local HOST=$TNL_SERVER
@@ -44,7 +47,7 @@ function tnlweb() {
 #  dns2ip netflix.com movies.netflix.com
 #
 function dns2ip() {
-	dig +short $* | sed "/[^0-9\.]/d" # use sed to remove non-IPv4 line e.g. alias
+  dig +short $* | sed "/[^0-9\.]/d" # use sed to remove non-IPv4 line e.g. alias
 }
 
 # tunnel specified domain(s) only.
@@ -53,8 +56,8 @@ function dns2ip() {
 #  tnlonly google.com
 #  tnlonly netflix.com movies.netflix.com
 #
-function tnlonly() { 
-	sshuttle -r $TNL_SERVER `dns2ip $*`;
+function tnlonly() {
+  sshuttle -vr $TNL_SERVER `dns2ip $*`;
 }
 
 # tunnel all traffic including DNS, except the specified domain(s).
@@ -63,16 +66,16 @@ function tnlonly() {
 #  tnlbut youku.com
 #  tnlbut youku.com weibo.com
 #
-function tnlbut() { 
-	sshuttle --dns -r $TNL_SERVER `dns2ip $* | sed "s/^/-x/"` 0/0; # use sed to append '-x' prefix 
+function tnlbut() {
+  sshuttle --dns -vr $TNL_SERVER `dns2ip $* | sed "s/^/-x/"` 0/0; # use sed to append '-x' prefix
 }
 
 # vpn to a ssh server.
-# 
+#
 # Examples:
 #  vpnto my_office_server      # host in ~/.ssh/config
 #  vpnto user@123.123.123.123
 #
 function vpnto() {
-	sshuttle -HNr $1;
+  sshuttle -HNvr $1;
 }
