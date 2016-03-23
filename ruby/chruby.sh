27d4cc15b5865260dd7d7da6f64eff5261bc1d45
Hector Parra <hector@parra.cat>
2016-03-23 12:23:25 +0100

    Auto-detect home-brew rubies

--- ruby/chruby.sh
+++ ruby/chruby.sh
@@ -1,5 +1,9 @@
 if [ -d "/usr/local/opt/chruby" ]; then
   source /usr/local/opt/chruby/share/chruby/chruby.sh
+  # add homebrew rubies
+  if [ -d "/usr/local/Cellar/ruby" ]; then
+    RUBIES+=(/usr/local/Cellar/ruby/*)
+  fi
   # enable auto-switching of Rubies specified by .ruby-version files
   source /usr/local/opt/chruby/share/chruby/auto.sh
 fi
