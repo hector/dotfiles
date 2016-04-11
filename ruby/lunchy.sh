de5b47cdd3963265a8ae3a9ca8561f3a344407c7
Hector Parra <hector@parra.cat>
2016-04-11 20:53:45 +0200

    Update lunchy file

--- ruby/lunchy.sh
+++ ruby/lunchy.sh
@@ -1,4 +1,6 @@
-LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
-if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
-  . $LUNCHY_DIR/lunchy-completion.bash
+if gem list lunchy -i >/dev/null 2>&1; then
+  LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
+  if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
+    . $LUNCHY_DIR/lunchy-completion.bash
+  fi
 fi
