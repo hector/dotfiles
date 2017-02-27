0e6ca75a24f138f56db96a1a8e5570224d13ad81
Hector Parra <hector@parra.cat>
2017-02-27 13:25:39 +0100

    Set default REACT_EDITOR

--- javascript.sh
+++ javascript.sh
@@ -1,5 +1,6 @@
-if which npm > /dev/null; then 
-	PATH=$PATH:/usr/local/share/npm/bin
+# Set the default editor to launch for the React red box
+if hash /Applications/WebStorm.app/Contents/MacOS/webstorm 2>&-; 
+then export REACT_EDITOR=/Applications/WebStorm.app/Contents/MacOS/webstorm
 fi
 
 if which yarn > /dev/null; then 
