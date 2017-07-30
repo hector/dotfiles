aec215fca9cd500e48958aee5ad36881d290e28b
Hector Parra <hector@parra.cat>
2017-07-30 20:19:50 +0200

    Update React editor

--- javascript.sh
+++ javascript.sh
@@ -1,6 +1,6 @@
 # Set the default editor to launch for the React red box
-if hash /Applications/WebStorm.app/Contents/MacOS/webstorm 2>&-; 
-then export REACT_EDITOR=/Applications/WebStorm.app/Contents/MacOS/webstorm
+if hash webstorm 2>&-; 
+then export REACT_EDITOR=webstorm
 fi
 
 if which yarn > /dev/null; then 
