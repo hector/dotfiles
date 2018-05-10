6ae99c3576c9a7dcaa5e02e55db3ab20868db23b
Hector Parra <hector@parra.cat>
2018-05-10 16:23:08 +0200

    Update homebrew clean

--- homebrew/config.sh
+++ homebrew/config.sh
@@ -3,7 +3,7 @@
 if hash brew 2>&-; then 
 	export PATH=`brew --prefix`/bin:`brew --prefix`/sbin:$PATH
   
-  alias brew-cleanall='brew cleanup && brew prune'
+  alias brew-cleanall='brew cleanup && brew prune && cask cleanup'
   
   export HOMEBREW_NO_ANALYTICS=1
   
@@ -12,9 +12,5 @@ if hash brew 2>&-; then
   export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
   alias cask='brew cask'
-  
-  # Temporal alias to upgrade casks (since 'brew cask upgrade' does not exist)
-  # Old version won't be automatically removed. Go to /opt/homebrew-cask/Caskroom to do so
-  alias brew-cask-upgrade='for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done'  
   
 fi
