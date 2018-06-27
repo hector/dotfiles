6333ca953adae6206f3410e8799eda4e0bd32e0e
Hector Parra <hector@parra.cat>
2018-06-27 12:25:11 +0200

    Add lunchy auto-completion

--- bash/completion.sh
+++ bash/completion.sh
@@ -5,4 +5,9 @@
 if [ -f "/usr/local/opt/bash-git-prompt/share/gitprompt.sh" ]; then
    __GIT_PROMPT_DIR="/usr/local/opt/bash-git-prompt/share"
    source "/usr/local/opt/bash-git-prompt/share/gitprompt.sh"
- fi
+fi
+
+LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
+if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
+  . $LUNCHY_DIR/lunchy-completion.bash
+fi
