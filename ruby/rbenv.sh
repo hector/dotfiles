c2d6a66a5c7b931a7d9793ed3681a0e452323d6e
Hector Parra <hector@parra.cat>
2018-09-03 17:28:06 +0200

    Remove old ruby switchers

--- ruby/rbenv.sh
+++
@@ -1,7 +0,0 @@
-if which rbenv > /dev/null; then
-	export RBENV_ROOT=/usr/local/var/rbenv
-	eval "$(rbenv init -)";
-elif [ -d "$HOME/.rbenv" ]; then
-	export PATH="$HOME/.rbenv/bin:$PATH"
-	eval "$(rbenv init -)";
-fi
