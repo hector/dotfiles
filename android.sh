c735bcdb43893bef77759a2841812056eae8e213
Hector Parra <hector@parra.cat>
2018-10-04 20:33:10 +0200

    Add Android Studio

--- android.sh
+++ android.sh
@@ -1,2 +1,9 @@
 # Android SDK
 export ANDROID_HOME=/usr/local/opt/android-sdk
+
+# Expo + Android Studio
+# https://docs.expo.io/versions/v30.0.0/workflow/android-studio-emulator.html
+if [ -d ~/Library/Android/sdk ]; then
+	export ANDROID_SDK=~/Library/Android/sdk
+	export PATH=~/Library/Android/sdk/platform-tools:$PATH
+fi
