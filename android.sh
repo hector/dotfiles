c3232fc7c91a0ed3fb1faefbb0502772e4bb8322
Hector Parra <hector@parra.cat>
2019-01-28 17:48:27 +0100

    Update android exports

--- android.sh
+++ android.sh
@@ -1,9 +1,11 @@
-# Android SDK
-export ANDROID_HOME=/usr/local/opt/android-sdk
-
-# Expo + Android Studio
-# https://docs.expo.io/versions/v30.0.0/workflow/android-studio-emulator.html
-if [ -d ~/Library/Android/sdk ]; then
-	export ANDROID_SDK=~/Library/Android/sdk
-	export PATH=~/Library/Android/sdk/platform-tools:$PATH
+# This is needed for:
+# 1. Expo + Android Studio: https://docs.expo.io/versions/v30.0.0/workflow/android-studio-emulator.html
+# 2. React Native + Android Studio: https://facebook.github.io/react-native/docs/getting-started
+if [ -d $HOME/Library/Android/sdk ]; then
+	export ANDROID_HOME=$HOME/Library/Android/sdk
+	export ANDROID_SDK=$HOME/Library/Android/sdk
+	export PATH=$PATH:$ANDROID_HOME/emulator
+	export PATH=$PATH:$ANDROID_HOME/tools
+	export PATH=$PATH:$ANDROID_HOME/tools/bin
+	export PATH=$PATH:$ANDROID_HOME/platform-tools
 fi
