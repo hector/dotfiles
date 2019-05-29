dc84dd700981cc076e4ff80623296bc26b472dee
Hector Parra <hector@parra.cat>
2019-05-29 17:08:20 +0100

    Use vscode as default editor

--- bash/editor.sh
+++ bash/editor.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Set the default editor to launch from terminal
-if hash subl 2>/dev/null; then export EDITOR='subl -w'
+if hash code 2>/dev/null; then export EDITOR='code -w'
+elif hash subl 2>/dev/null; then export EDITOR='subl -w'
 elif hash mate 2>/dev/null; then export EDITOR='mate -w'
 elif hash nano 2>/dev/null; then export EDITOR=nano
 elif hash vim 2>/dev/null; then export EDITOR=vim	
