40c125f302d4a8cc7a647cce1a5efe9878dc9a23
Hector Parra <hector@parra.cat>
2019-11-21 11:39:40 +0300

    Add emscripten

---
+++ emscripten.sh
@@ -0,0 +1,9 @@
+# Instructions from: brew install emscripten
+#export LLVM=/usr/local/opt/emscripten/libexec/llvm/bin
+
+if [ -d $HOME/.my_installs/emsdk ]; then
+	export PATH=$PATH:$HOME/.my_installs/emsdk
+    export PATH=$PATH:$HOME/.my_installs/emsdk/upstream/emscripten
+    export PATH=$PATH:$HOME/.my_installs/emsdk/node/12.9.1_64bit/bin
+    source $HOME/.my_installs/emsdk/emsdk_env.sh --build=Release > /dev/null
+fi
