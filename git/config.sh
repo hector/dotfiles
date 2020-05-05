410f782315771791729e069e0e9c4400bead7f44
Hector Parra <hector@parra.cat>
2020-05-05 14:49:22 +0300

    Use Visual Studio Code as git editor

--- git/config.sh
+++ git/config.sh
@@ -4,8 +4,9 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
 
-if hash subl 2>/dev/null; then export GIT_EDITOR='subl -w'
-elif hash mate 2>/dev/null-; then export GIT_EDITOR="mate -w"
+if hash code 2>/dev/null; then export GIT_EDITOR='code -w'
+elif hash subl 2>/dev/null; then export GIT_EDITOR='subl -w'
+elif hash mate 2>/dev/null; then export GIT_EDITOR="mate -w"
 elif hash nano 2>/dev/null; then export GIT_EDITOR=nano
 elif hash vim 2>/dev/null; then export GIT_EDITOR=vim	
 else export GIT_EDITOR=vi
